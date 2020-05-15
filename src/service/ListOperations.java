c85e4fb0431965186d35628d8653b816c6780bec
Varsha <hp@DESKTOP-4IENAI5>
2020-05-15 21:27:02 +0530

    FSJ0420A113

--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,42 @@
 package service;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import model.*;
+
+public class ListOperations
+{
+	List<TouristPlace> placesList= new  ArrayList<TouristPlace>();
+	public List<TouristPlace> add(TouristPlace places)
+	{
+		placesList.add(places);
+		return placesList;
+		
+	}
+	public List<TouristPlace> remove(TouristPlace places)
+	{
+		placesList.remove(places);
+		return placesList;
+		
+	}
+	public Object sortByDestination(List<TouristPlace> places)
+	{
+		Collections.sort(places,(d1,d2)-> d1.getDestination().compareTo(d2.getDestination()));
+		return places;
+		
+	}
+	public Object sortByRank(List<TouristPlace> places)
+	{
+		Collections.sort(places,(d1,d2)-> d1.getRank().compareTo(d2.getRank()));
+		return places;
+		
+	}
+	public Object reset(List<TouristPlace> places)
+	{
+		places.clear();
+		return places;
+		
+	}
+
+}
--- src/service/MapOperations.java
+++ src/service/MapOperations.java
@@ -1,2 +1,51 @@
 package service;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
+import model.*;
+
+public class MapOperations
+{
+	
+	
+	public Map<Integer,TouristPlace> add(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> tp=new HashMap<>();
+		tp.put(null, places);
+		return tp;
+	}
+	public Map<Integer,TouristPlace> sortRandomly(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> sr=new HashMap<>();
+		sr.put(null, places);
+		return sr;
+	}
+	public Map<Integer,TouristPlace> sortInEntryOrder(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> se=new LinkedHashMap<>();
+		se.put( null, places);
+		return se;
+		
+	}
+	public Map<Integer,TouristPlace> sortAlphabetically(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> sa=new TreeMap<>();
+		sa.put( null, places);
+		return sa;	
+	}
+	public Object reset(HashMap<Integer, TouristPlace> places)
+	{
+		Map<Integer,TouristPlace> resetPlaces=new TreeMap<>();
+		resetPlaces.clear();
+		return resetPlaces;
+	}
+	public Map<Integer,TouristPlace> remove(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> rp=new HashMap<>();
+		rp.remove( null, places);
+		return rp;
+	}
+
+}
