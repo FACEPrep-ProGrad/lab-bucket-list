8d88bdacda6a5c3dda3bc321f26df1e862c6052d
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-09-13 12:22:09 +0530

    FSJ0820B107

--- src/service/MapOperations.java
+++ src/service/MapOperations.java
@@ -1,2 +1,44 @@
 package service;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import model.TouristPlace;
+
+public class MapOperations{
+	
+	
+	HashMap<String,TouristPlace> map = new HashMap<String,TouristPlace>();
+	
+	public HashMap<String,TouristPlace> add(TouristPlace places){
+		
+		map.put(places.getName(), places);
+		return map;
+	}
+	
+	public HashMap<String,TouristPlace> sortRandomly(TouristPlace places) {
+		return add(places);
+	}
+	
+	public HashMap<String,TouristPlace> sortInEntryOrder(HashMap<String,TouristPlace> places) {
+		
+		return null;
+	}
+	
+	public TreeMap<String,TouristPlace>  sortAlphabetically(HashMap<String,TouristPlace> places) {
+		
+		TreeMap<String, TouristPlace>  SortedPlaces = new TreeMap<String,TouristPlace>(places);
+		return SortedPlaces;
+	}
+	
+	public Object reset(HashMap<String,TouristPlace> places) {
+		places.clear();
+		return places;
+	}
+	
+	public HashMap<String,TouristPlace> remove(TouristPlace places) {
+		
+		map.remove(places.getName());
+		return null;
+	}
+}
--- src/service/SetOperations.java
+++ src/service/SetOperations.java
@@ -1,2 +1,50 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import model.TouristPlace;
+
+
+public class SetOperations{
+	
+	HashSet<TouristPlace> bucketList = new HashSet<TouristPlace>();
+	
+	public HashSet<TouristPlace> add(TouristPlace places){
+		
+		bucketList.add(places);
+		return bucketList;
+	}
+	
+	public HashSet<TouristPlace> remove(TouristPlace places){
+		System.out.println("Remove this id:");
+		System.out.println(places);
+		bucketList.remove(places);
+		return bucketList;
+	}
+	
+	public Object sortByDestination(HashSet<TouristPlace> places) {
+		
+		ArrayList<TouristPlace> bucketList = new ArrayList<TouristPlace>(places);
+		
+		bucketList.sort((TouristPlace p1, TouristPlace p2) -> p1.getDestination().compareTo(p2.getDestination()) );
+		
+		return new LinkedHashSet<TouristPlace>(bucketList);
+	}
+	
+	public Object sortByRank(HashSet<TouristPlace> places) {
+		
+		ArrayList<TouristPlace> bucketList = new ArrayList<TouristPlace>(places);
+		
+		bucketList.sort((TouristPlace p1, TouristPlace p2) -> p1.getRank().compareTo(p2.getRank()));
+		
+		return new LinkedHashSet<TouristPlace>(bucketList);	
+	}
+	
+	public Object reset(HashSet<TouristPlace> places) {
+		
+		places.clear();
+		
+		return places;
+	}
+}
