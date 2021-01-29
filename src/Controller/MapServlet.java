e554dcbd0834cf1bdf6881de099fdd89165f92a0
Vinod Kumar Reddy <vinodkumarreddy@MacBook-Air-4.local>
2021-01-29 12:24:08 +0530

    1722022

--- src/Controller/MapServlet.java
+++ src/Controller/MapServlet.java
@@ -17,10 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.TouristPlace;
 import service.ListOperations;
+import service.MapOperations;
 
 
 @WebServlet(urlPatterns= {"/map"})
 public class MapServlet extends HttpServlet {
+	Map<String,TouristPlace> map = new HashMap<String, TouristPlace>();
 
 	private static final long serialVersionUID = 1L;
 
@@ -35,48 +37,54 @@ public class MapServlet extends HttpServlet {
 		String sortAlphabetically = request.getParameter("sortalphabetically");
 		String remove = request.getParameter("delete");
 		String reset = request.getParameter("reset");
+		
+		TouristPlace touristPlace=new TouristPlace(name,destination,rank);
+		
+		MapOperations mapOperations=new MapOperations();
+		
 			
 		if(add!=null) {
-			// call the add method and store the return value in a map variable
 			
-			request.setAttribute("bucketList", /*return variable */);
+			request.setAttribute("bucketList", mapOperations.add(touristPlace));
+			map.put(touristPlace.getName(), touristPlace);
 			request.setAttribute("message", "user added successfully");
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
 
 		if(remove!=null) {
-			// call the remove method and store the return value in a map variable
-			request.setAttribute("bucketList", /*return variable */);
+			
+			
+			request.setAttribute("bucketList",mapOperations.remove(touristPlace) );
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
 
 		
 		if(sortRandom!=null) {
-			// call the sortRandomly method and store the return value in a map variable
-			request.setAttribute("bucketList",/*return variable */);
+			
+			request.setAttribute("bucketList",mapOperations.sortRandomly(touristPlace));
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
 
 		if(sortInEntryOrder!=null) {
-			// call the sortInEntryOrder and store the return value in a map variable
-			request.setAttribute("bucketList", /*return variable */);
+			
+			request.setAttribute("bucketList",mapOperations.sortInEntryOrder((HashMap)map) );
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
 		if(sortAlphabetically!=null) {
 			
-			// call the sort Alphabetically and store the return value in a map variable
-			request.setAttribute("bucketList",/*return variable */);
+			
+			request.setAttribute("bucketList",mapOperations.sortAlphabetically((HashMap)map));
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
 
 		if(reset!=null) {	
-			// call the reset method and store the return value in a map variable
-			request.setAttribute("bucketList", /*return variable */);
+			
+			request.setAttribute("bucketList", mapOperations.reset((HashMap) map));
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/map.jsp");
 			rd.forward(request, response);
 		}
--- src/service/SetOperations.java
+++ src/service/SetOperations.java
@@ -1,2 +1,39 @@
 package service;
+import java.util.*;
 
+import model.TouristPlace;
+public class SetOperations{
+	ArrayList<TouristPlace> list = new ArrayList<TouristPlace>();
+	HashSet<TouristPlace> set = new HashSet<TouristPlace>();
+	public  HashSet add(TouristPlace places) {
+		set.add(places);
+		return set;
+		
+	}
+	public HashSet remove(TouristPlace places) {
+		set.remove(places);
+		
+		return set;
+		
+	}
+	public Object sortByDestination(TouristPlace places) {
+		list.sort((TouristPlace p1,TouristPlace p2)->p1.getDestination().compareTo(p2.getDestination()));
+		return list;
+		
+	}
+	public Object sortByRank(TouristPlace places) {
+		
+		list.sort((TouristPlace p1,TouristPlace p2)->p1.getRank().compareTo(p2.getRank()));
+
+		return list;
+		
+	}
+	public Object reset(HashSet places) {
+		set.clear();
+		return set;
+		
+	}
+	
+	
+	
+}
